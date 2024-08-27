a148f4e81be73f39a9eb1d80c9a962c88da145ac
Kseniya <zhytariuk15012005@gmail.com@>
2024-08-28 00:37:04 +0300

    init commit

---
+++ lr1/dist/functions.js
@@ -0,0 +1,7 @@
+"use strict";
+function foo(stringValue, intValue) {
+    if (intValue === void 0) { intValue = 10; }
+    console.log(stringValue + intValue);
+}
+foo('int');
+//# sourceMappingURL=functions.js.map
